d7f650f1d36308bc674f9e2323ff7bebd266dd58
FreeL21 <antoniinmoreno@gmail.com>
2020-01-28 10:39:18 +0100

    app.route / user param / Search + Filters / Obj to Json / Result printing

--- src/services.py
+++ src/services.py
@@ -75,3 +75,54 @@ def init_data():
     CATALOG_LIST = pyongc.listObjects()
     print("DONE: data downloaded." + str(len(CATALOG_LIST)) + "objects loaded")
 
+@app.route("/", methods=['GET'])
+def ongc_list():
+
+    global CATALOG_LIST
+    global SEARCH_WHITE_LIST
+    result = []
+
+    # get user request param
+    filters = request.args if request.args else []
+
+    # Filter fields verification
+    for filter in filters:
+        if filter not in SEARCH_WHITE_LIST:
+            msg = "The parameter [" \
+                  + filter \
+                  + "] does not exist in filter parameters. The available filters are : " \
+                  + ", ".join(map(str, SEARCH_WHITE_LIST))
+            return msg, status.HTTP_400_BAD_REQUEST
+
+    # Search by filters
+    if len(filters):
+        # In all objects
+        for obj in CATALOG_LIST:
+            found = False
+            # For each filters
+            for filter in filters.items():
+                # filters are peers
+                filter_name = filter[0]
+                # Cast the value in the good type
+                if filter[1].find('\"') != -1:  # string
+                    filter_value = filter[1].strip('\"')
+                elif filter[1].find('.') != -1:  # float
+                    filter_value = float(filter[1])
+                else:  # int
+                    filter_value = int(filter[1])
+                if getattr(obj, "_" + filter_name) == filter_value:
+                    found = True
+                    break
+            # Add the object just once
+            if found:
+                result.append(obj)
+    else:
+        result = CATALOG_LIST
+
+    # Object to Json
+    serialized_result = [response_template(obj) for obj in result]
+    return serialized_result
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
